5fe6ab4c0bb731d0993d43b3e309052e7506f488
Kasanka <osanka@yandex.ru>
2019-09-03 14:29:28 +0300

    Выполнено задание 13

--- Задание 12/js/script.js
+++ Задание 12/js/script.js
@@ -108,7 +108,50 @@ window.addEventListener('DOMContentLoaded', function() {
 
         statusMessage.classList.add('status');
 
-    form.addEventListener('submit', function(event) {
+    function sendForm(elem) {
+        elem.addEventListener('submit', function (event) {
+            event.preventDefault();
+            elem.appendChild(statusMessage);
+
+            let formData = new FormData(elem);
+
+            function postData() {
+                return new Promise(function (resolve, reject) {
+                    let request = new XMLHttpRequest();
+                    request.open('POST', 'server.php');
+                    request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+                    request.onreadystatechange = function() {
+                        if (request.readyState < 4) {
+                            resolve();
+                        } else if (request.readyState === 4) {
+                            if (request.status == 200 && request.status < 300) {
+                                resolve();
+                            } else {
+                                reject();
+                            }
+                        }
+                    };
+                    request.send(formData);
+                });
+            }
+
+            function clearInput() {
+                for (let i=0; i < input.length; i++) {
+                    input[i].value = '';
+                }
+            }
+
+            postData(formData)
+                .then (() => statusMessage.innerHTML = message.loading)
+                .then (() => statusMessage.innerHTML = message.success)
+                .catch (() => statusMessage.innerHTML = message.failure)
+                .then (clearInput);
+        });      
+    }
+    
+    sendForm(form);
+
+    /*form.addEventListener('submit', function(event) {
         event.preventDefault();
         form.appendChild(statusMessage);
 
@@ -139,7 +182,7 @@ window.addEventListener('DOMContentLoaded', function() {
         for (let i = 0; i < input.length; i++) {
             input[i].value = '';
         }
-    });
+    });*/
 });
 
 //Второе задание
